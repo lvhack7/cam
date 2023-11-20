35815b78f4f09ae9e1b94d03caa5e513c88e5dcc
Khaknazar Abilda <hacki117@Khaknazars-MacBook-Air.local>
2023-11-20 11:23:40 +0600

    fixes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function isMobile() {
+    // Set the breakpoint at which you consider the device to be mobile
+    const mobileWidth = 768; // Adjust this value based on your design needs
+  
+    // Check if the viewport width is less than the mobileWidth
+    return window.innerWidth < mobileWidth;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const carousel = document.getElementById("carousel-license");
     const inner = carousel.querySelector(".carousel-inner");
@@ -5,23 +13,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let currentIndex = 0;
 
-    function updateCarouselLicense() {
-        slides.forEach((slide, index) => {
-            const offset = index - currentIndex - 1;
-            slide.style.opacity = Math.abs(offset) <= 1 ? 1 : 0; // Show only the three adjacent slides
-        });
+    window.addEventListener("resize", function(event) {
+        updateCarouselLicense()
+    })
 
-        inner.style.transform = `translateX(-${currentIndex * (100 / 3)}%)`;
+    function updateCarouselLicense() {
+        if (isMobile()) {
+            slides.forEach((slide, index) => {
+                const offset = index - currentIndex;
+                slide.style.opacity = Math.abs(offset) < 1 ? 1 : 0; // Show only the three adjacent slides
+            });
+
+            inner.style.transform = `translateX(-${currentIndex * (100)}%)`;
+        } else {
+            slides.forEach((slide, index) => {
+                const offset = index - currentIndex - 1;
+                slide.style.opacity = Math.abs(offset) <= 1 ? 1 : 0; // Show only the three adjacent slides
+            });
+
+            inner.style.transform = `translateX(-${currentIndex * (100/3)}%)`;
+        }
     }
 
     function nextSlideLicense() {
-        currentIndex = (currentIndex + 1) % (slides.length - 2); // Subtract 2 for the number of visible slides
-        updateCarouselLicense();
+        if (isMobile()) {
+            currentIndex = (currentIndex + 1) % (slides.length); // Subtract 2 for the number of visible slides
+            updateCarouselLicense();
+        } else {
+            currentIndex = (currentIndex + 1) % (slides.length - 2); // Subtract 2 for the number of visible slides
+            updateCarouselLicense();    
+        }
     }
 
     function prevSlideLicense() {
-        currentIndex = (currentIndex - 1 + (slides.length - 2)) % (slides.length - 2);
-        updateCarouselLicense();
+        if (isMobile()) {
+            currentIndex = (currentIndex - 1 + (slides.length)) % (slides.length);
+            updateCarouselLicense();
+        } else {
+            currentIndex = (currentIndex - 1 + (slides.length - 2)) % (slides.length - 2);
+            updateCarouselLicense();
+        }
+        
     }
 
     document.getElementById("prev-btn-license").addEventListener("click", prevSlideLicense);
@@ -38,23 +70,47 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let currentIndex = 0;
 
-    function updateCarouselLicense() {
-        slides.forEach((slide, index) => {
-            const offset = index - currentIndex - 1;
-            slide.style.opacity = Math.abs(offset) <= 1 ? 1 : 0; // Show only the three adjacent slides
-        });
+    window.addEventListener("resize", function(event) {
+        updateCarouselLicense()
+    })
 
-        inner.style.transform = `translateX(-${currentIndex * (100 / 3)}%)`;
+    function updateCarouselLicense() {
+        if (isMobile()) {
+            slides.forEach((slide, index) => {
+                const offset = index - currentIndex;
+                slide.style.opacity = Math.abs(offset) < 1 ? 1 : 0; // Show only the three adjacent slides
+            });
+
+            inner.style.transform = `translateX(-${currentIndex * (100)}%)`;
+        } else {
+            slides.forEach((slide, index) => {
+                const offset = index - currentIndex - 1;
+                slide.style.opacity = Math.abs(offset) <= 1 ? 1 : 0; // Show only the three adjacent slides
+            });
+
+            inner.style.transform = `translateX(-${currentIndex * (100/3)}%)`;
+        }
     }
 
     function nextSlideLicense() {
-        currentIndex = (currentIndex + 1) % (slides.length - 2); // Subtract 2 for the number of visible slides
-        updateCarouselLicense();
+        if (isMobile()) {
+            currentIndex = (currentIndex + 1) % (slides.length); // Subtract 2 for the number of visible slides
+            updateCarouselLicense();
+        } else {
+            currentIndex = (currentIndex + 1) % (slides.length - 2); // Subtract 2 for the number of visible slides
+            updateCarouselLicense();    
+        }
     }
 
     function prevSlideLicense() {
-        currentIndex = (currentIndex - 1 + (slides.length - 2)) % (slides.length - 2);
-        updateCarouselLicense();
+        if (isMobile()) {
+            currentIndex = (currentIndex - 1 + (slides.length)) % (slides.length);
+            updateCarouselLicense();
+        } else {
+            currentIndex = (currentIndex - 1 + (slides.length - 2)) % (slides.length - 2);
+            updateCarouselLicense();
+        }
+        
     }
 
     document.getElementById("prev-btn-obj").addEventListener("click", prevSlideLicense);
@@ -104,23 +160,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let currentIndex = 0;
 
-    function updateCarouselLicense() {
-        slides.forEach((slide, index) => {
-            const offset = index - currentIndex - 1;
-            slide.style.opacity = Math.abs(offset) <= 1 ? 1 : 0; // Show only the three adjacent slides
-        });
+    window.addEventListener("resize", function(event) {
+        updateCarouselLicense()
+    })
 
-        inner.style.transform = `translateX(-${currentIndex * (100 / 3)}%)`;
+    function updateCarouselLicense() {
+        if (isMobile()) {
+            slides.forEach((slide, index) => {
+                const offset = index - currentIndex;
+                slide.style.opacity = Math.abs(offset) < 1 ? 1 : 0; // Show only the three adjacent slides
+            });
+
+            inner.style.transform = `translateX(-${currentIndex * (100)}%)`;
+        } else {
+            slides.forEach((slide, index) => {
+                const offset = index - currentIndex - 1;
+                slide.style.opacity = Math.abs(offset) <= 1 ? 1 : 0; // Show only the three adjacent slides
+            });
+
+            inner.style.transform = `translateX(-${currentIndex * (100/3)}%)`;
+        }
     }
 
     function nextSlideLicense() {
-        currentIndex = (currentIndex + 1) % (slides.length - 2); // Subtract 2 for the number of visible slides
-        updateCarouselLicense();
+        if (isMobile()) {
+            currentIndex = (currentIndex + 1) % (slides.length); // Subtract 2 for the number of visible slides
+            updateCarouselLicense();
+        } else {
+            currentIndex = (currentIndex + 1) % (slides.length - 2); // Subtract 2 for the number of visible slides
+            updateCarouselLicense();    
+        }
     }
 
     function prevSlideLicense() {
-        currentIndex = (currentIndex - 1 + (slides.length - 2)) % (slides.length - 2);
-        updateCarouselLicense();
+        if (isMobile()) {
+            currentIndex = (currentIndex - 1 + (slides.length)) % (slides.length);
+            updateCarouselLicense();
+        } else {
+            currentIndex = (currentIndex - 1 + (slides.length - 2)) % (slides.length - 2);
+            updateCarouselLicense();
+        }
+        
     }
 
     document.getElementById("prev-btn-letter").addEventListener("click", prevSlideLicense);
@@ -129,4 +209,41 @@ document.addEventListener("DOMContentLoaded", function () {
     // Initialize the carousel
     updateCarouselLicense();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const mobileLinks = document.querySelectorAll(".mobile-link")
+
+    mobileLinks.forEach((link, index) => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault()
+            
+            let href = link.dataset.href
+            //console.log(href)
+            window.location.href = href
+        })
+    })
+})
+
+function toggleMenu() {
+    var mobileMenu = document.getElementById('mobile-menu');
+    var body = document.body
+
+    if(mobileMenu.classList.contains("hidden")) {
+        mobileMenu.classList.remove("hidden")
+        body.classList.add("overflow-hidden")
+    } else {
+        mobileMenu.classList.add("hidden")
+        body.classList.remove("overflow-hidden")
+    }
+}
+
+function contact() {
+    var contact = document.getElementById("contact-panel")
+
+    if (contact.classList.contains("hidden")) {
+        contact.classList.remove("hidden")
+    } else {
+        contact.classList.add("hidden")
+    }
+}
 
---
+++ survey-list/script.js
@@ -0,0 +1,44 @@
+const list = [
+    "Bypass level indicator",
+    "Customised logos on dials of pointer measuring instruments",
+    "Diaphragm seals",
+    "DirectDrive",
+    "External chambers",
+    "Float switches, horizontal mounting",
+    "Glass level gauge, glass tube, LGG-GA",
+    "Glass level gauge, glass tube, LGG-GB",
+    "Glass level gauge, high-pressure version, transparent and reflex version",
+    "Glass level gauge, reflex version, LGG-E",
+    "Glass level gauge, refraction version",
+    "Glass level gauge, transparent and reflex version",
+    "Glass level gauge, weld-in version, reflex version",
+    "Level transmitters",
+    "Primary flow elements",
+    "Questionnaire for float switches, vertical installation",
+    "Reactor multipoints, to determine linear temperature profiles",
+    "Reactor multipoints, to determine radial temperature profiles",
+    "Restriction orifices",
+    "Sampling probe/injection quill, for monoblock models IBS3 and IBJ4",
+    "Thermowells",
+    "Tubeskin sensor application"
+]
+
+document.addEventListener("DOMContentLoaded", function() {
+    const wika = document.getElementById("wika")
+
+    list.forEach((item, index) => {
+        let newElement = document.createElement('div')
+        newElement.innerHTML = `
+        <div class="card shadow bg-white p-6 h-[200px]">
+            <div class="flex flex-col justify-between h-full">
+                <div class="text-base font-medium">${item}</div>
+                <div class="flex items-center space-x-2">
+                    <p class="open font-light text-gray-700 uppercase">Открыть</p>
+                    <div class="line border-b border-1 border-gray-500 w-[100px]"></div>
+                </div>
+            </div>
+        </div>
+        `
+        wika.appendChild(newElement)
+    })
+})
